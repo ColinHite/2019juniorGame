ea13ebbf9c646309c93effdc50432e3c7104b083
Unknown <colin.hite@gmail.com>
2018-05-01 10:16:13 -0600

    Got the documentation for some scripts done
    I went through and updated any of my scripts that I know I made so that they can more easily be used by someone else.

--- TheGameGame/Assets/Scripts/Character_Player/AttackTrail_Jim_Hite.cs
+++ TheGameGame/Assets/Scripts/Character_Player/AttackTrail_Jim_Hite.cs
@@ -1,4 +1,9 @@
-﻿using System.Collections;
+﻿/// <summary>
+/// This script controls the on and off state for the attack VFX trail used on Jim's model
+/// 
+/// Author: Colin Hite
+/// </summary>
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,16 +11,20 @@ public class AttackTrail_Jim_Hite : MonoBehaviour {
 		
 	//public int VFX_array_index;
 
+	//This is used to keep track of the prefabs uded on the axe
 	public GameObject[] attackTrail;
 
+	//Public reference to class elementManager
 	public ElementManager elementManager;
 
 	public void Start()
 	{
+		//Find the player and get its script
 		elementManager = GameObject.FindWithTag("Player").GetComponent<ElementManager>();
 		print("Attack VFX " + elementManager);
 	}
 
+	//This method is used to call the series of events needed for the trail to appear and disappear
 	public void TrailVFX_Call()
 	{
 		//checks ElementManager for current element to determine what element VFX to use
@@ -23,11 +32,13 @@ public class AttackTrail_Jim_Hite : MonoBehaviour {
 		print("element number " + ((int)elementManager.currentElement - 1));
 	}
 
+	//This method turns the effect trail on
 	public void TrailVFX_On(GameObject currentVFX)
 	{
 		currentVFX.SetActive (true);
 		StartCoroutine (attackTrail_Off (currentVFX));
 	}
+	//This method is a timer to turn the trail off.
 	public IEnumerator attackTrail_Off(GameObject VFXpass)
 	{
 		yield return new WaitForSeconds (0.9f);
--- TheGameGame/Assets/Scripts/Character_Player/VFX_Jim_HammerSmash_Hite.cs
+++ TheGameGame/Assets/Scripts/Character_Player/VFX_Jim_HammerSmash_Hite.cs
@@ -1,28 +1,39 @@
-﻿using System.Collections;
+﻿/// <summary>
+/// This script controls the effect timing and use of Jim's hammer down charge attack.
+/// 
+/// Author: Colin Hite
+/// </summary>
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class VFX_Jim_HammerSmash_Hite : MonoBehaviour {
 
+	//This keeps track of all jim's prefabs needed to do the hammer charge attack.
 	public GameObject[] attackDown;
 
 	//public int VFX_array_index;
 
+	//This line defines the parent used to reset the effect to the player object
 	public GameObject vfx_parent;
 
+	//Public reference to class elementManager
 	public ElementManager elementManager;
 
 	public void Start()
 	{
+		//Find the player and get its script
 		elementManager = GameObject.FindWithTag("Player").GetComponent<ElementManager>();
 	}
 
+	//This method is used to call the series of events needed to do the hammer charge attack
 	public void HammerVFX_Call()
 	{
 		//checks ElementManager for current element to determine what element VFX to use
 		HammerVFX_On (attackDown [(int)elementManager.currentElement - 1]);
 	}
-
+	//This method turns the effect on
 	public void HammerVFX_On(GameObject currentVFX)
 	{
 		//yield return new WaitForSeconds (0.1f);
@@ -30,6 +41,7 @@ public class VFX_Jim_HammerSmash_Hite : MonoBehaviour {
 		currentVFX.transform.SetParent (null);
 		StartCoroutine (HammerVFX_Off (currentVFX));
 	}
+	//This method turns the effect off
 	public IEnumerator HammerVFX_Off(GameObject VFXpass)
 	{
 		yield return new WaitForSeconds (7.0f);
